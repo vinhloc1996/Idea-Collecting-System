fe8caa2ec9fa61301c6fd09113f507e38c8d5d45
Nguyễn Bảo Vĩnh Lộc <vinhloc1996@yahoo.com.vn>
2018-02-07 10:32:42 +0700

    Add project files.

---
+++ Idea Collecting System/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Idea_Collecting_System.MvcApplication" Language="C#" %>
---
+++ Idea Collecting System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Idea_Collecting_System.Startup))]
+namespace Idea_Collecting_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
